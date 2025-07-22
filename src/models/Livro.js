3d762922de41104c191d3af61b7b3990a394721f
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-07-22 12:28:01 -0300

    Criando um validador global e lidando com strings vazias e outros erros nas classes

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,12 +14,16 @@ const livroSchema = new mongoose.Schema(
     },
     editora: {
       type: String,
-      required: [true, "A editora é obrigatória"]
+      required: [true, "A editora é obrigatória"],
+      enum: {
+        values: ["Casa do Código", "Alura"], 
+        message: "A editora não é um valor permitido"
+      }
     },
     numeroPaginas: {
         type: Number,
-        min: 10, 
-        max: 6000
+        min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"], 
+        max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
     }
   }
 );
